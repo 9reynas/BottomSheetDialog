e0cbed446bbb2a1db62ee98c5b05f24bf6fd0a3d
9reynas <31100946+9reynas@users.noreply.github.com>
2018-03-25 15:31:39 +0900

    update1

--- app/src/main/java/proyect/my/of/example/miyuki/bottomsheetdialog/MainActivity.java
+++ app/src/main/java/proyect/my/of/example/miyuki/bottomsheetdialog/MainActivity.java
@@ -3,6 +3,7 @@ package proyect.my.of.example.miyuki.bottomsheetdialog;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
         bottomSheetDialog.setContentView(getLayoutInflater().inflate(R.layout.bottomsheet_dialog,null));
         bottomSheetDialog.show();
+
+        findViewById(R.id.buttompress).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.show();
+            }
+        });
+
+
+
     }
 
 }
